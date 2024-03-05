08f52b07b9dd7dc3ec0c6feca67a4e804428dc9f
gergo <113334790+gergogyulai@users.noreply.github.com>
2024-03-05 21:14:51 +0100

    Refactor project card component

--- components/project-card.tsx
+++ components/project-card.tsx
@@ -1,13 +1,6 @@
-"use client"
-
 import { Chip } from "@/components/chips";
 import TechIcon from "@/components/tech-icon";
-import {
-  HoverCard,
-  HoverCardContent,
-  HoverCardTrigger,
-} from "@/components/ui/hover-card"
-
+import React from "react";
 
 interface ProjectCardProps {
     title: string;
@@ -18,37 +11,30 @@ interface ProjectCardProps {
 }
 
 export const ProjectCard: React.FC<ProjectCardProps> = ({ title, description, status, link, tech }) => {
-    const handleClick = () => {
-        if (link) {
-            // Redirect to the provided link if available
-            window.open(link, "_blank");
-        }
-    };
-
     return (
         <>
-        {/* <HoverCard>
-            <HoverCardTrigger> */}
-                <div className="">
-                    <div className="inline-flex gap-2">
-                        <p className={link ? "cursor-pointer font-medium underline underline-offset-2" : "font-medium underline underline-offset-2"} onClick={handleClick}>
+            <div className="">
+                <div className="inline-flex gap-2">
+                    {link ? (
+                        <a href={link} target="_blank" rel="noopener noreferrer" className="cursor-pointer font-medium underline underline-offset-2">
+                            {title}
+                        </a>
+                    ) : (
+                        <p className="font-medium underline underline-offset-2">
                             {title}
                         </p>
-                        <Chip status={status} />
-                        <div className="flex h-6 items-center justify-center gap-2">
-                            {tech && tech.map((item, index) => (
-                                <TechIcon key={index} name={item} />
-                            ))}
-                        </div>
+                    )}
+                    <Chip status={status} />
+                    <div className="flex h-6 items-center justify-center gap-2">
+                        {tech && tech.map((item, index) => (
+                            <TechIcon key={index} name={item} />
+                        ))}
                     </div>
-                    <p className="font-medium text-black/60 dark:text-white/60">
-                        {description}
-                    </p>
                 </div>
-            {/* </HoverCardTrigger>
-            <HoverCardContent>
-            </HoverCardContent>
-        </HoverCard> */}
+                <p className="font-medium text-black/60 dark:text-white/60">
+                    {description}
+                </p>
+            </div>
         </>
     );
 }
